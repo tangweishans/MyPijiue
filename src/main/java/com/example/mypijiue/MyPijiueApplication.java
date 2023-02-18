7f40d1e0d48e03bbbfca1d9fb4c3f9cd55523562
tws <924504178@qq.com>
2023-02-18 19:02:57 +0800

    Initial commit

---
+++ src/main/java/com/example/mypijiue/MyPijiueApplication.java
@@ -0,0 +1,13 @@
+package com.example.mypijiue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPijiueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPijiueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mypijiue/MyPijiueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mypijiue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPijiueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
